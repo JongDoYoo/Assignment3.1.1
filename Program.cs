d308ed476b5d95a6eba30d397c0dc33fea470ca1
JongD <JongD@JONGDO>
2024-05-21 06:09:27 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Assignment3._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 100; i= i+2)
+            {
+                Console.WriteLine(i);
+            }
+            
+                
+                
+                
+          
+                    
+        }
+    }
+}
